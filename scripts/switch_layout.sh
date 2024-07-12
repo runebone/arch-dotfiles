60655dfff5806a3f00a3d1bc98f621384296e6e3
Konstantin Runov <runebone1@gmail.com>
2024-07-12 21:06:48 +0300

    add some scripts from nixos

---
+++ scripts/switch_layout.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+current_variant=$(setxkbmap -query | grep variant | awk '{print $2}')
+
+if [ "$current_variant" = "dvorak," ]; then
+    setxkbmap -layout us,ru
+else
+    setxkbmap -layout us,ru -variant dvorak,
+    xmodmap ~/.dotfiles/xmodmaps/rpd.xmodmap
+fi
