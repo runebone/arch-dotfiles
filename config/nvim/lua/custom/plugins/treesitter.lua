ce72fdf629822614952874fb78e4a9f500235bf8
Konstantin Runov <runebone1@gmail.com>
2024-09-06 18:19:25 +0300

    update config and packages

--- config/nvim/lua/custom/plugins/treesitter.lua
+++ config/nvim/lua/custom/plugins/treesitter.lua
@@ -30,7 +30,7 @@ end
 
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+    dependencies = { "nvim-treesitter/nvim-treesitter-context", "nvim-treesitter/nvim-treesitter-textobjects" },
     build = ":TSUpdate",
     config = setup
 }
