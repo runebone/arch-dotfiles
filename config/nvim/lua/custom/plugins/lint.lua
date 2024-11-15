861c86eb7db1c97bd8190584e9364cb70e50fc98
Konstantin Runov <runebone1@gmail.com>
2024-11-15 11:58:41 +0300

    add golang-ci-lint

---
+++ config/nvim/lua/custom/plugins/lint.lua
@@ -0,0 +1,17 @@
+local setup = function()
+    local lint = require('lint')
+    lint.linters_by_ft = {
+        go = { "golangcilint" }
+    }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+            require("lint").try_lint()
+        end,
+    })
+end
+
+return {
+    "mfussenegger/nvim-lint",
+    config = setup
+}
