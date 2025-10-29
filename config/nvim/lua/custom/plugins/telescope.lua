5daeb2f67d350e6dcb0049631c5199651e64a441
Konstantin Runov <k.runov@etc.ltd>
2025-10-29 12:25:07 +0300

    feat: Live Grep by word under cursor + fuzzy find by file name (full path) (OMG THAT'S SO SICK!!!)

--- config/nvim/lua/custom/plugins/telescope.lua
+++ config/nvim/lua/custom/plugins/telescope.lua
@@ -18,10 +18,36 @@ local setup = function()
     vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
 
     vim.keymap.set('n', '<leader>j', function()
-      vim.cmd('normal! yiw')
-      local text = vim.fn.getreg('+')
-      builtin.live_grep({ default_text = text })
-    end, { desc = "Telescope live_grep с текстом из буфера обмена" })
+      local pickers = require('telescope.pickers')
+      local finders = require('telescope.finders')
+      local conf = require('telescope.config').values
+      local make_entry = require('telescope.make_entry')
+
+      local word = vim.fn.expand('<cword>')
+
+      local function make_finder()
+        local args = vim.deepcopy(conf.vimgrep_arguments)
+        table.insert(args, '--fixed-strings')
+        table.insert(args, word)
+        local default_em = make_entry.gen_from_vimgrep({})
+        return finders.new_oneshot_job(args, {
+          entry_maker = function(line)
+            local entry = default_em(line)
+            if not entry then return nil end
+            local rel = vim.fn.fnamemodify(entry.filename, ':.')
+            entry.ordinal = rel
+            return entry
+          end,
+        })
+      end
+
+      pickers.new({}, {
+        prompt_title = "Live Grep: '" .. word .. "' | Find Files",
+        finder = make_finder(),
+        sorter = conf.generic_sorter({}),
+        previewer = conf.grep_previewer({}),
+      }):find()
+    end, { desc = 'Live grep by word under cursor with filter by full path' })
 end
 
 return {
