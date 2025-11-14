7ebcc503802875c57baf0d0b071d75139eccb29c
Konstantin Runov <k.runov@etc.ltd>
2025-11-14 18:44:36 +0300

    add open last screenshot script

--- scripts/open-last-screenshot.sh
+++ scripts/open-last-screenshot.sh
@@ -3,5 +3,6 @@
 
 LAST_SCREENSHOT="$(ls -t ~/Pictures/Screenshots 2>/dev/null | head -n 1)"
 if [ -n "$LAST_SCREENSHOT" ]; then
+    wl-copy < "$HOME/Pictures/Screenshots/$LAST_SCREENSHOT"
     xdg-open "$HOME/Pictures/Screenshots/$LAST_SCREENSHOT"
 fi
