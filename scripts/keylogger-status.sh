be5cf19baebe7efa95ecd34e920413270c6aca25
Konstantin Runov <k.runov@etc.ltd>
2026-07-02 18:06:36 +0300

    waybar

--- scripts/keylogger-status.sh
+++ scripts/keylogger-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PID_FILE="/tmp/keylogger.pid"
 if [ -f "$PID_FILE" ] && kill -0 "$(cat "$PID_FILE")" 2>/dev/null; then
-    echo '{"text": " ", "class": "active", "tooltip": ""}'
+    echo '{"text": " ", "class": "active", "tooltip": "active"}'
 else
-    echo '{"text": " ", "class": "inactive", "tooltip": ""}'
+    echo '{"text": " ", "class": "inactive", "tooltip": "inactive"}'
 fi
